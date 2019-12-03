9c5ab34eb3b0f2e425753ae4c3dcab08f36457d1
AdrianaRosas <salamipizza8909@gmail.com>
2019-12-02 23:46:24 -0600

    Maquetación de Register y Home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-import Home from './Components/Home/Home';
+// import Home from './Components/Home/Home';
+import Welcome from './Components/Home/Home'
 import Register from './Components/Register/Register'
 
 
@@ -10,8 +11,9 @@ import Register from './Components/Register/Register'
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Route exact path='/' component={Register} />
-      <Route  path='/home' component={Home} />
+      <Route exact path='/register' component={Register} />
+      <Route  path='/home' component={Welcome} />
+
 
     
     </Router>
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react';
 import './Register.css';
-import Button from '@material-ui/core/Button';
+// import { input } from '@material-ui/core';
+import TransitionsModal from '../Modal/Modal'
+import { Button } from '@material-ui/core';
 
-
-import Input from '../utils/Input'
 
 class Register extends Component {
+    render(){
+       return (
+           <section className="section-register">
+               <div className="register-tittle">
+                  <label>Registra tu entrada</label>
+               </div>
+               <div class="register-container">
+                   <input type="text" name="name" placeholder="Nombre completo"/>
+                   <input type="text" name="name" placeholder="Correo electrónico"/>
+                   <input type="text" name="name" placeholder="Persona/empresa a la que visita"/>
+               </div>
+               <div className="needs">
+               <label><input type='radio' name='Color' value='Red' /></label>
+                   <p class="txt-needs">¿Tienes alguna discapacidad o requieres asistencia?</p>
+               </div>
+               <div className="warn">
+                   <p class="txt-warn">Al continuar con tu registro, aceptas nuestra política de privacidad</p>
+               </div>
+                   <div className="privacity">
+                     <TransitionsModal/>
+                   </div>
+                   <div className="sig">
+                     <Button variant="contained">Entrada</Button>
+                   </div>
 
-    render() {
-        var style = {
-            width:'15rem', 
-            height:'4vh', 
-            borderRadius:'10px', 
-            textAlign:'center',
-            marginTop:'2rem'
-           }
-        return (
-            <section className="section-register">
-                <Input
-                    placeholder="Introduce tu nombre"
-                    style={style}
-                    
-                />
-                <Input
-                    placeholder="¿a quién visitas?"
-                    style={style}
-                    
-                />
-                <label>¿eres discapacitado?</label>
-                <button>Si</button><button>No</button>
-                <button>Enviar respuesta</button>
-                <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-        
-            </section>
-        )}}
-        
 
- export default Register;
+           </section>
+
+
+
+       )
+
+    }
+
+    
+}
+export default Register;
