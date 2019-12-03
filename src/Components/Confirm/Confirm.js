c5d1fc31a89ebcdb0481e7f87510329e81e0b159
Isis Lázaro <isislazaro@gmail.com>
2019-12-03 10:43:35 -0600

    depurando código comentado

--- src/Components/Confirm/Confirm.js
+++ src/Components/Confirm/Confirm.js
@@ -10,23 +10,25 @@ class ConfirmData extends Component {
         return (
             <section className="section-confirm">
                 <div className="confirm-tittle">
-                   <label>Confirma tus datos</label>
+                    <label>Confirma tus datos</label>
                 </div>
                 <div className="confirm-btn">
-                   <div className="btn-col1">
-                   <Link to="/">
-                   <Button variant="contained">Confirmar</Button>
-                   </Link>
-                   </div>
-                   <div className="btn-col1">
-                   <Link to="/register">
-                   <Button variant="contained">Regresar</Button>
-                   </Link>
-                   </div>
+                    <div className="btn-col1">
+                        <Link to="/">
+                            <Button variant="contained">Confirmar</Button>
+                        </Link>
+                    </div>
+                    <div className="btn-col1">
+                        <Link to="/register">
+                            <Button variant="contained">Regresar</Button>
+                        </Link>
+                    </div>
                 </div>
-               
-            </section> 
-        )}}
-        
 
- export default ConfirmData;
+            </section>
+        )
+    }
+}
+
+
+export default ConfirmData;
